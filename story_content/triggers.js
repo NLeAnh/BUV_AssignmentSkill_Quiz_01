ca6c01b41af6a8960c9ab97c475c0110683029e0
NLeAnh <anh.nl2@buv.edu.vn>
2025-05-28 10:35:30 +0700

    Upload file 05

---
+++ story_content/triggers.js
@@ -0,0 +1,69 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6hlt8QdqQ4D":
+        Script1();
+        break;
+      case "6l6A148kFPL":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('5h94G7pmpEd');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '5z8fpbMQKAh';
+const pulseAmount = 0.1;
+const delay = 2797;
+addToTimeline(
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script2 = function()
+{
+  const target = object('5h94G7pmpEd');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '5z8fpbMQKAh';
+const pulseAmount = 0.1;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
